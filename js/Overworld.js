c53e8d29ac018936dfb3098e18020bec8557b154
Erbol Zhaparov <ae270718@gmail.com>
2024-02-03 20:37:00 +0600

    added new map, gameloop, basic movements by x and y position

--- js/Overworld.js
+++ js/Overworld.js
@@ -3,36 +3,40 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector('.game-canvas');
     this.ctx = this.canvas.getContext('2d');
+    this.map = null;
+  }
+  
+  startGameLoop() {
+    const step = () => {
+      
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      
+      //draw lower layer
+      this.map.drawLowerImage(this.ctx);
+      
+      //draw game objects
+      Object.values(this.map.gameObjects).forEach(object => {
+        /*object.x += 0.01;*/
+        /*object.y += 0.01;*/
+        object.sprite.draw(this.ctx);
+      })
+      
+      //draw upper layer
+      this.map.drawUpperImage(this.ctx);
+      
+      requestAnimationFrame(() => {
+        step();
+      })
+    }
+    step();
   }
   
   init() {
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-    image.src = '/images/maps/DemoLower.png';
+    this.map = new OverworldMap(window.OverworldMaps.Kitchen);
+    this.startGameLoop();
     
-    //place some game objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    })
-    const npc1 = new GameObject({
-      x: 7,
-      y: 9,
-      src: '/images/characters/people/npc1.png'
-    })
-    const npc2 = new GameObject({
-      x: 5,
-      y: 8,
-      src: '/images/characters/people/npc2.png'
-    })
     
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-      npc2.sprite.draw(this.ctx);
-    }, 200)
+  
     
     
   }
